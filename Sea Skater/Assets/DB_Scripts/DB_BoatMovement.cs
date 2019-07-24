13086e8fe7b7d20cbc47a4475738b02c6bba8648
David Brooks <davebrooks199909@gmail.com>
2019-07-24 12:19:42 +0100

    Further Movement detections and Out Of Bounds Corrections
    In this commit i decided that before working on the environment of the game my movement should be better than just moving left and right.
    Although the movement worked fine and it did what it was made for.
    I would have eventually needed to make the character move up and down.
    Swipping up to jump and down to slide.
    
    Although at the moment i do not have any assets to use with animations to give the effect of jumping and diving.
    So i used the players transform and scale to give the effect of these stats of play which will later be exacuted.
    I made the Player Object smaller when the player swips down so that the player can fit under object in the way.
    Swip up for when the player needs to jump over the objects.
    
    These obsticles are not in place yet however will be soon.
    
    I also decided to clamp the player. This needed to be done as i didnt want the player moving out of bounds which was doable in the new movement system.
    As there were no restraints on how far the player can move left or right

--- Sea Skater/Assets/DB_Scripts/DB_BoatMovement.cs
+++ Sea Skater/Assets/DB_Scripts/DB_BoatMovement.cs
@@ -11,13 +11,20 @@ public class DB_BoatMovement : MonoBehaviour
     public Transform player;
     // swip to touch Variables 
     public Vector3 startTouchPoisition, endTouchPosition;
+    private float dragDistance;
 
+    // Movement Variables 
+    // Used to change how high the player jumps
+    public float jumpHeight = 3f;
+    // Used to determine how long the sliding animation will be played for
+    public float slideTime = 2f;
 
 	// Use this for initialization
 	void Start ()
     {
         // Finding the players position
         player = GameObject.Find("Player").GetComponent<Transform>();
+        dragDistance = Screen.height * 15 / 100;    // dragDistance is 15% height of the screen
 
 	}
 	
@@ -27,6 +34,12 @@ public class DB_BoatMovement : MonoBehaviour
         // Functions
         BoatMovement();
         SwipToMove();
+
+        Vector3 clampedPos = transform.position;
+        clampedPos.x = Mathf.Clamp(clampedPos.x, -2, 2);
+        transform.position = clampedPos;
+
+
         #region Stopping Test
         // This Test shows that when the player stop so does the boat. This will work both ways. 
         // Boat dont move player dont move. Player dont move boat dont move it keeps the value of its distance
@@ -38,26 +51,102 @@ public class DB_BoatMovement : MonoBehaviour
         {
             playerMovementSpeed = 0;        
         }
-        Debug.Log(TestTimer);
+        //Debug.Log(TestTimer);
         #endregion
     }
 
     // Function which allows for the inputs when the player swips the screen in 4 directions
     void SwipToMove()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            startTouchPoisition = Input.GetTouch(0).position;
+        // Simple way to move left and right however we need to make sure the game knows when we swip down or up
+        // Also we need to declare these areas on the screen of any mobile device.
+        #region Old Swip Movement
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //    startTouchPoisition = Input.GetTouch(0).position;
 
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        //if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        //{
+        //    endTouchPosition = Input.GetTouch(0).position;
+
+        //    if ((endTouchPosition.x < startTouchPoisition.x) && transform.position.x > -2)
+        //        transform.position = new Vector3(transform.position.x - 2, transform.position.y, gameObject.transform.position.z);
+
+        //    if ((endTouchPosition.x > startTouchPoisition.x) && transform.position.x < 2)
+        //        transform.position = new Vector3(transform.position.x + 2, transform.position.y, gameObject.transform.position.z);
+        //}
+        #endregion
+
+        #region New Swip Movement
+        if(Input.touchCount == 1)   // Player only touches the screen 1 time
         {
-            endTouchPosition = Input.GetTouch(0).position;
-
-            if ((endTouchPosition.x < startTouchPoisition.x) && transform.position.x > -2)
-                transform.position = new Vector3(transform.position.x - 2, transform.position.y, gameObject.transform.position.z);
-
-            if ((endTouchPosition.x > startTouchPoisition.x) && transform.position.x < 2)
-                transform.position = new Vector3(transform.position.x + 2, transform.position.y, gameObject.transform.position.z);
+            Touch touch = Input.GetTouch(0);    // Get the touch
+            if(touch.phase == TouchPhase.Began) // Check to see if the player has pressed the screen yet
+            {
+                // set up our Vector3 positions for the start position the player pushed on the screen 
+                startTouchPoisition = touch.position;
+                // Until the player releases their finger the end touch wont be found
+                endTouchPosition = touch.position;
+            }
+            // Update the last position based on where the player moved
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                endTouchPosition = touch.position;
+            }
+            // Check if the players finger is removed from the screen
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                endTouchPosition = touch.position;  // Last toch is found now
+                // reset the scale of the player character when a new touch has been omitted
+                // We can remove this later when the animation for sliding is made
+                player.localScale = new Vector3(player.localScale.x, 1, player.localScale.z);
+                player.position = new Vector3(player.position.x, .5f, player.position.z);
+                // Check if the drag distance is greater than 20% of the screen hieght
+                if (Mathf.Abs(endTouchPosition.x - startTouchPoisition.x) > dragDistance || Mathf.Abs(endTouchPosition.y - startTouchPoisition.y) > dragDistance)
+                {
+                    // check to see if the swip is vertical or horizontal
+                    if(Mathf.Abs(endTouchPosition.x - startTouchPoisition.x) > Mathf.Abs(endTouchPosition.y - startTouchPoisition.y))
+                    {
+                        // if the horizontal movement is greater than the vertical movement
+                        if((endTouchPosition.x > startTouchPoisition.x))
+                        {
+                            // move right 2 within the X axis as we are swiping right
+                            transform.position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+                            Debug.Log("Right Swip"); 
+                        }
+                        // however if the horizontal movement is less than the vertical movement
+                        else
+                        {
+                            // we move left -2 within the X axis as we are swiping left
+                            transform.position = new Vector3(transform.position.x - 2, transform.position.y, transform.position.z);
+                            Debug.Log("Swip Left");
+                        }
+                    }
+                    else
+                    {
+                        // if the Vertical movement is greater than the horizontal movement
+                        if(endTouchPosition.y > startTouchPoisition.y)
+                        {
+                            player.position = new Vector3(player.position.x, player.position.y + jumpHeight, player.position.z);
+                            Debug.Log("Up Swip");
+                        }
+                        // else vertical movement is less than the horizontal movement
+                        else
+                        {
+                            // Make into an animation later on so it returns back to normal. 
+                            // This is the sliding effect
+                            player.localScale = new Vector3(player.localScale.x, -0.3f, player.localScale.z);
+                            Debug.Log("Down Swipe");
+                        }
+                    }
+                }
+                else
+                {
+                    // Its a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
+            }
         }
+        #endregion
     }
 
     void BoatMovement()
@@ -70,6 +159,14 @@ public class DB_BoatMovement : MonoBehaviour
         Debug.Log(distance);
         transform.position = (transform.position - player.position).normalized * distance + player.position;
         player.position = new Vector3(gameObject.transform.position.x, player.position.y, player.position.z);
+        if (player.position.y > .4f)
+        {
+            transform.position = new Vector3(transform.position.x, .4f, transform.position.z);
+        }
+        if(player.localScale.y < 1)
+        {
+            transform.position = new Vector3(transform.position.x, .4f, transform.position.z);
+        }
 
         // Set the distance so the player can move from behind
         player.transform.Translate(0, 0, playerMovementSpeed * Time.deltaTime);
